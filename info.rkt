281e9f5e53dc4ea65d700ca1d77f419b4a2d6398
leiDnedyA <aydendiel@gmail.com>
2024-01-13 16:26:41 -0500

    fix: removed all racket library files copied over from rs-universe repo

--- info.rkt
+++
@@ -1,9 +0,0 @@
-#lang info
-(define collection "racketscript-universe")
-(define deps '("base" "racketscript/base" "racketscript-compiler"))
-(define build-deps '("scribble-lib" "racket-doc"))
-(define scribblings '(("scribblings/racketscript-universe.scrbl" ())))
-(define pkg-desc "Implementation of htdp/universe in RacketScript with PeerJS")
-(define version "0.0")
-(define pkg-authors '(leiDnedyA))
-(define license '(Apache-2.0 OR MIT))
--- main.rkt
+++
@@ -1,7 +0,0 @@
-#lang racketscript/base
-
-(require "./src/universe.rkt")
-(provide (all-from-out "./src/universe.rkt"))
-
-;; TODO
-;; implement starting/stopping big bang and universe
