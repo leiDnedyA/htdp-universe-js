d989f0619d17aff8e28fdedc9ed2fc43fe5612b1
leiDnedyA <aydendiel@gmail.com>
2024-02-01 17:18:23 -0500

    feat: started adding documentation for bigBang and toDraw functions

---
+++ docs-input/main.js
@@ -0,0 +1,21 @@
+/*
+ *
+ * This file exists so that JSDoc can be used to generate
+ * documentation. It should only contain function headers
+ * and corresponding JSDoc comments.
+ *
+ * */
+
+/** 
+  * Starts a world program in the initial state defined by initWorld.
+  * @param {*} initWorld - The initial state for the world program to be run.
+  * @param {...*} handlers - The event handlers for the given world
+  */
+function bigBang(initWorld, ...handlers) {}
+
+/**
+ * Tells bigBang to call "renderFunction" whenever an image must
+ * be drawn to the canvas.
+ * @param {function} renderFunction - The function to be called on each render
+ */ 
+function toDraw(renderFunction) {}
